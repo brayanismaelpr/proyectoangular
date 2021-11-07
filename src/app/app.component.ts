8f0dbf5c1b499e94e2776107f7bcb48c36e3c1c5
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2021-11-07 10:44:54 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectoangular';
+  title = 'Proyecto Angular';
+  saludo = 'Saludo desde Angular';
 }
